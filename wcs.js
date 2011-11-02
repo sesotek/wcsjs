48be70ddca78d68debb73cbf407696af47bbec78
Ian Smith <imsmith@uw.edu>
2011-11-01 23:06:56 -0700

    Basic trig functions and very rudimentary check for supported transformation.

--- wcs.js
+++ wcs.js
@@ -14,13 +14,53 @@
   
     WCS.version = '0.0.1';
 
-    WCS.Mapper = function (header) {
+    var validTransforms = ['RA---TAN', 'DEC--TAN'];
+    
+    // Trig
+    function degToRad(deg) {
+        return ((2 * Math.PI * deg) / 360);
+    }
+    function radToDeg(rad) {
+        return (360 * rad / (2 * Math.PI));
+    }
+    function sind(deg) {
+        return Math.sin(degToRad(deg));
+    }
+    function cosd(deg) {
+        return Math.cos(degToRad(deg));
+    }
+    function tand(deg) {
+        return Math.tan(degToRad(deg));
+    }
+    function arcsind(deg) {
+        return Math.asin(degToRad(deg));
+    }
+    function arccosd(deg) {
+        return Math.acos(degToRad(deg));
+    }
+    function arctand(deg) {
+        return Math.atan(degToRad(deg));
+    }
 
-        this.pixelToCoordinate(x, y) {
+    function inArray(arr, obj) {
+        return (arr.indexOf(obj) != -1);
+    }
+    
+    function supportedTransform(header) {
+       return (inArray(validTransforms, header['CTYPE1']) && inArray(validTransforms, header['CTYPE2']));
+    }
+
+    WCS.Mapper = function (header) {
+        
+        if (!supportedTransform(header)) {
+            throw('Unsupported transformation.');
+        }
+
+        this.pixelToCoordinate = function(x, y) {
 
         }
 
-        this.coordinateToPixel(ra, dec) {
+        this.coordinateToPixel = function(ra, dec) {
 
         }
     }
